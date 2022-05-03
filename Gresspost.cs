e8b2d55b0e0bed674f22c314fdbbb0e1780173f9
Cobolt-60 <93054720+Cobolt-60@users.noreply.github.com>
2022-05-03 11:21:39 +0200

    Create Gresspost.cs

---
+++ Gresspost.cs
@@ -0,0 +1,3 @@
+//Wie gehts? ich habe kaine anung.
+// phfipshgsfgvfhsivhsipfvpkrpivnpisbnvpibrpibvsipbvipsbrvpihbrpivbspiberpvisribvrspibv This was made possible by type script.
+// Tahts all good bye.
